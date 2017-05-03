a20f224468e5de2b3153d87d84360d0bc86eda05
John Pavlicek <john@johnpavlicek.com>
2017-05-03 15:58:13 -0400

    Pass Leap tests

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,13 @@
-def is_leap_year():
-    pass
+def is_leap_year(year):
+    # Check for div by 4
+    if (year % 4 == 0):
+        # Check for div by 100
+        if (year % 100 == 0):
+            # Check for div by 400
+            if (year % 400 == 0):
+                return True
+        else:
+            return True
+    
+    #Default to False
+    return False
