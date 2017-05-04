1592d8d2232b2ac7b6ef477b2859e1213b5319db
John Pavlicek <john@johnpavlicek.com>
2017-05-04 19:14:57 -0400

    Successful pass of isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,9 +1,22 @@
+import string
+
 def is_isogram(str):
-    str=str.replace(' ','')
-    setstr = set(str.upper())
-    lisstr = list(str.upper())
-    if (len(setstr)==len(lisstr)):
+    # COnvert to uppercase to find upper and lower character repeats
+    strlist = list(str.upper())
+
+    # create empty list
+    testlist = []
+
+    # Check each character against all ascii uppercase characters
+    for char in strlist:
+        if (char in string.ascii_uppercase):
+            # add to list if a letter
+            testlist.append(char)
+
+    # Get the set of characters in the list
+    testset = set(testlist)
+
+    # If length of the set and length of list are the same, is isogram
+    if (len(testlist)==len(testset)):
         return True
-    print setstr
-    print lisstr
     return False
