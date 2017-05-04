76ecbaedca26686043eab40fd03d76c667055999
John Pavlicek <john@johnpavlicek.com>
2017-05-04 18:52:53 -0400

    One test remaining

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,6 +1,9 @@
 def is_isogram(str):
+    str=str.replace(' ','')
     setstr = set(str.upper())
     lisstr = list(str.upper())
     if (len(setstr)==len(lisstr)):
         return True
+    print setstr
+    print lisstr
     return False
