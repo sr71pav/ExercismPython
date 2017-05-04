d8831e876266bfb4735dd8791d4f7b7381a1943d
John Pavlicek <john@johnpavlicek.com>
2017-05-04 18:45:23 -0400

    Second try at isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,6 +1,6 @@
 def is_isogram(str):
-    setstr = set(str)
-    lisstr = list(str)
+    setstr = set(str.upper())
+    lisstr = list(str.upper())
     if (len(setstr)==len(lisstr)):
         return True
     return False
