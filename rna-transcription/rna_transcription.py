f2e1f7545534f095dc397ea57de45cde517310b8
John Pavlicek <john@johnpavlicek.com>
2017-05-08 19:15:53 -0400

    Pass RNA transcription test

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,10 @@
-def to_rna():
-    pass
+def to_rna(strin):
+    for char in strin:
+        if (char != 'G') and (char != 'C') and (char != 'A') and (char != 'T'):
+            return ''
+    strin = strin.replace('G','-')
+    strin = strin.replace('C','G')
+    strin = strin.replace('A','U')
+    strin = strin.replace('T','A')
+    strin = strin.replace('-','C')
+    return strin
