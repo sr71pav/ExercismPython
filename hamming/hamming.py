cef1356c45c31a3f183f9e095f266682f2d2b509
John Pavlicek <john@johnpavlicek.com>
2017-05-18 20:44:39 -0400

    Pass hamming tests

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,8 @@
-def distance():
-    pass
+def distance(strand1, strand2):
+    if (len(strand1) != len(strand2)):
+        raise ValueError
+    count = 0
+    for i in xrange(len(strand1)):
+        if (strand1[i] != strand2[i]):
+            count+=1
+    return count
