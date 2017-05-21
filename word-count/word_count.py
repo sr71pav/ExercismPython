07b5e419d2c35cdede043edc7b09a2c70b7576c1
John Pavlicek <john@johnpavlicek.com>
2017-05-20 22:03:12 -0400

    Pass word count test

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,13 @@
-def word_count():
-    pass
+import string
+import re
+
+def word_count(strin):
+    res = {}
+    strin = re.sub('[^A-Za-z0-9]', ' ', strin)
+    strlist = string.split(strin.lower())
+    for word in strlist:
+        if word in res:
+            res[word] += 1
+        else:
+            res[word] = 1
+    return res
